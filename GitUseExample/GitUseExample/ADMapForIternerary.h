2e3a902d7ff520fc4587e5c763c61ca836912736
HolyenZou <holyenzou@gmail.com>
2013-04-24 11:58:32 +0800

    started work on new map feature,not quite done yet.

---
+++ GitUseExample/GitUseExample/ADMapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  ADMapForIternerary.h
+//  GitUseExample
+//
+//  Created by Holyen Zou on 13-4-24.
+//  Copyright (c) 2013年 AnyData.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADMapForIternerary : NSObject
+
+@end
---
+++ GitUseExample/GitUseExample/ADMapForIternerary.m
@@ -0,0 +1,13 @@
+//
+//  ADMapForIternerary.m
+//  GitUseExample
+//
+//  Created by Holyen Zou on 13-4-24.
+//  Copyright (c) 2013年 AnyData.com. All rights reserved.
+//
+
+#import "ADMapForIternerary.h"
+
+@implementation ADMapForIternerary
+
+@end
